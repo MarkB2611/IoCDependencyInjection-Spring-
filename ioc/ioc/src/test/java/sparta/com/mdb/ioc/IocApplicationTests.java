8cc00bdb6230429091a8b23562e49961a85e816d
Mark <markblackmore26@gmail.com>
2025-08-12 10:11:43 +0100

    added
    added spring workspace

---
+++ ioc/ioc/src/test/java/sparta/com/mdb/ioc/IocApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.com.mdb.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
